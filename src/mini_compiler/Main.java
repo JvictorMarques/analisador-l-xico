1285b55e1c49758efeb7fe92c89fc34dc1940d80
mariliasantos21m <mariliasantosnas21@gmail.com>
2025-09-15 16:21:25 -0300

    [FEAT]: Adiciona nome dos integrantes no main

--- src/mini_compiler/Main.java
+++ src/mini_compiler/Main.java
@@ -4,7 +4,14 @@ import lexical.Scanner;
 import lexical.Token;
 
 public class Main {
-	
+	/*
+	* GRUPO
+	* DIMAS LIMA DA COSTA
+	* JOÃO VICTOR MARQUES DA SILVA
+	* MARÍLIA SANTOS DO NASCIMENTO
+	* NATHYANE OLIVEIRA
+	* THIAGO VASCONCELOS
+	* */
 	public static void main(String[] args) throws Exception {
 		Scanner sc = new Scanner("programa.mc");
 		Token tk;
